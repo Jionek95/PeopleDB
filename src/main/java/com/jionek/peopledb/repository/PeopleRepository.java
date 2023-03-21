0c5b7a2c07df2cb93c635d20e6acfe5605dbe17e
Jionek95 <jakopiczko@wp.pl>
2023-03-21 14:55:12 +0100

    Create save() method in PeopleRepository class

--- src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
+++ src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
@@ -1,4 +1,9 @@
 package com.jionek.peopledb.repository;
 
+import com.jionek.peopledb.model.Person;
+
 public class PeopleRepository {
+    public Person save(Person person) {
+        return person;
+    }
 }
