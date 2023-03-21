6f5025fb31b0a8c54e72ec132eb3766f2aed87f8
Jionek95 <jakopiczko@wp.pl>
2023-03-21 14:42:24 +0100

    Create PeopleRepositoryTests and PeopleRepository classes

--- src/test/java/com/jionek/peopledb/repository/PeopleRepositoryTests.java
+++ src/test/java/com/jionek/peopledb/repository/PeopleRepositoryTests.java
@@ -1,4 +1,11 @@
 package com.jionek.peopledb.repository;
 
+import org.junit.jupiter.api.Test;
+
 public class PeopleRepositoryTests {
+
+    @Test
+    public void canSave(){
+        PeopleRepository repo = new PeopleRepository();
+    }
 }
