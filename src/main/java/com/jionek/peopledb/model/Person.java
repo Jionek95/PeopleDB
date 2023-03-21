f752a298ac405720367ffe04ac76e170478d48fc
Jionek95 <jakopiczko@wp.pl>
2023-03-21 15:18:21 +0100

    Create id property in class Person

--- src/main/java/com/jionek/peopledb/model/Person.java
+++ src/main/java/com/jionek/peopledb/model/Person.java
@@ -3,6 +3,15 @@ package com.jionek.peopledb.model;
 import java.time.ZonedDateTime;
 
 public class Person {
+    private Long id;
     public Person(String firstName, String lastName, ZonedDateTime dob) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
