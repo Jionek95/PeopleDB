0e37b60656ddb1a251f9f31436a469a6e8e6fe98
Jionek95 <jakopiczko@wp.pl>
2023-03-22 16:00:41 +0100

    add canSaveTwoPeople() to PeopleRepositoryTests

--- src/test/java/com/jionek/peopledb/repository/PeopleRepositoryTests.java
+++ src/test/java/com/jionek/peopledb/repository/PeopleRepositoryTests.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PeopleRepositoryTests {
 
     @Test
-    public void canSave(){
+    public void canSaveOnePerson(){
         PeopleRepository repo = new PeopleRepository();
         Person john = new Person(
                 "John", "Smith", ZonedDateTime.of(1980, 11, 15 , 15,15,0,0, ZoneId.of("-6"))
@@ -19,4 +19,18 @@ public class PeopleRepositoryTests {
         Person savedPerson = repo.save(john);
         assertThat(savedPerson.getId()).isGreaterThan(0);
     }
+
+   @Test
+   public void canSaveTwoPeople(){
+       PeopleRepository repo = new PeopleRepository();
+       Person john = new Person(
+               "John", "Smith", ZonedDateTime.of(1980, 11, 15 , 15,15,0,0, ZoneId.of("-6"))
+       );
+       Person bobby = new Person(
+               "Bobby", "Horse", ZonedDateTime.of(1985, 11, 25 , 15,15,12,0, ZoneId.of("-6"))
+       );
+       Person savedPerson1 = repo.save(john);
+       Person savedPerson2 = repo.save(bobby);
+       assertThat(savedPerson1.getId()).isNotEqualTo(savedPerson2.getId());
+   }
 }
