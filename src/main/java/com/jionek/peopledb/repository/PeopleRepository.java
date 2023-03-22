3ddaaf2a7b68d2946d64d23696a76b16dfd47332
Jionek95 <jakopiczko@wp.pl>
2023-03-22 16:33:06 +0100

    use Connection in PeopleRepository class

--- src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
+++ src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
@@ -2,8 +2,17 @@ package com.jionek.peopledb.repository;
 
 import com.jionek.peopledb.model.Person;
 
+import java.sql.Connection;
+
 public class PeopleRepository {
+    private Connection connection;
+    public PeopleRepository(Connection connection) {
+        this.connection = connection;
+    }
+
     public Person save(Person person) {
+        String sql = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES";
+        connection.prepareStatement(sql);
         return person;
     }
 }
