f1c0fd21126045cbb51a9e5f99702a4d61829f53
Jionek95 <jakopiczko@wp.pl>
2023-03-24 18:44:50 +0100

    Make String sql as constant in PeopleRepository class

--- src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
+++ src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
@@ -6,15 +6,15 @@ import java.sql.*;
 import java.time.ZoneId;
 
 public class PeopleRepository {
+    public static final String SAVE_PERSON_SQL = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES(?, ?, ?)";
     private Connection connection;
     public PeopleRepository(Connection connection) {
         this.connection = connection;
     }
 
     public Person save(Person person) {
-        String sql = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES(?, ?, ?)";
         try {
-            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = connection.prepareStatement(SAVE_PERSON_SQL, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, person.getFirstName());
             ps.setString(2, person.getLastName());
             ps.setTimestamp(3, Timestamp.valueOf(person.getDob().withZoneSameInstant(ZoneId.of("+0")).toLocalDateTime()));
