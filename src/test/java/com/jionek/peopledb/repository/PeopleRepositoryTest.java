412ea7293baa5963d1289da11726dc45a045f8d9
Jionek95 <jakopiczko@wp.pl>
2023-04-03 16:08:23 +0200

    Update experiment() test method

--- src/test/java/com/jionek/peopledb/repository/PeopleRepositoryTest.java
+++ src/test/java/com/jionek/peopledb/repository/PeopleRepositoryTest.java
@@ -117,6 +117,11 @@ public class PeopleRepositoryTest {
     }
 @Test
     public void experiment(){
+        Person p1 = new Person(10L, null, null, null);
+        Person p2 = new Person(20L, null, null, null);
+        Person p3 = new Person(30L, null, null, null);
+        Person p4 = new Person(40L, null, null, null);
+        Person p5 = new Person(50L, null, null, null);
 
 }
 
