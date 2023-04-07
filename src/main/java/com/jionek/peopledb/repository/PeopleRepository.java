b9e185fca2b5bbb2555826042fcd8050fb514947
Jionek95 <jakopiczko@wp.pl>
2023-04-07 19:41:46 +0200

    update MapForSave method and comment whole save method

--- src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
+++ src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
@@ -31,29 +31,31 @@ public class PeopleRepository extends CRUDRepository<Person> {
 
     @Override
     void mapForSave(Person person, PreparedStatement ps) throws SQLException {
-
+        ps.setString(1, person.getFirstName());
+        ps.setString(2, person.getLastName());
+        ps.setTimestamp(3, convertDobToTimestamp(person.getDob()));
     }
 
-    public Person save(Person person) throws UnableToSaveException{
-        try {
-            PreparedStatement ps = connection.prepareStatement(SAVE_PERSON_SQL, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, person.getFirstName());
-            ps.setString(2, person.getLastName());
-            ps.setTimestamp(3, convertDobToTimestamp(person.getDob()));
-            int recordsAffected = ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys();
-            while (rs.next()){
-                long id = rs.getLong(1);
-                person.setId(id);
-                System.out.println(person);
-            }
-            System.out.printf("Records affected: %d%n", recordsAffected);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new UnableToSaveException("Tried to save person: " + person);
-        }
-        return person;
-    }
+//    public Person save(Person person) throws UnableToSaveException{
+//        try {
+//            PreparedStatement ps = connection.prepareStatement(SAVE_PERSON_SQL, Statement.RETURN_GENERATED_KEYS);
+//            ps.setString(1, person.getFirstName());
+//            ps.setString(2, person.getLastName());
+//            ps.setTimestamp(3, convertDobToTimestamp(person.getDob()));
+//            int recordsAffected = ps.executeUpdate();
+//            ResultSet rs = ps.getGeneratedKeys();
+//            while (rs.next()){
+//                long id = rs.getLong(1);
+//                person.setId(id);
+//                System.out.println(person);
+//            }
+//            System.out.printf("Records affected: %d%n", recordsAffected);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            throw new UnableToSaveException("Tried to save person: " + person);
+//        }
+//        return person;
+//    }
 
     public Optional<Person> findById(Person person) {
 
