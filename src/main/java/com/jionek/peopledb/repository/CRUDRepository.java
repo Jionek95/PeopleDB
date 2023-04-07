f1d4a4814e22861c5d69c765118f261787b5d699
Jionek95 <jakopiczko@wp.pl>
2023-04-07 19:36:10 +0200

    T class in genetic type for CRUDRepository extends created Entity interface

--- src/main/java/com/jionek/peopledb/repository/CRUDRepository.java
+++ src/main/java/com/jionek/peopledb/repository/CRUDRepository.java
@@ -1,33 +1,34 @@
 package com.jionek.peopledb.repository;
 
 import com.jionek.peopledb.exception.UnableToSaveException;
+import com.jionek.peopledb.model.Entity;
 
 import java.sql.*;
 
-abstract class CRUDRepository <T> {
+abstract class CRUDRepository <T extends Entity> {
     protected Connection connection;
 
     public CRUDRepository(Connection connection) {
         this.connection = connection;
     }
 
-    public T save(T person) throws UnableToSaveException {
+    public T save(T entity) throws UnableToSaveException {
         try {
             PreparedStatement ps = connection.prepareStatement(getSaveSql(), Statement.RETURN_GENERATED_KEYS);
-            mapForSave(person, ps);
+            mapForSave(entity, ps);
             int recordsAffected = ps.executeUpdate();
             ResultSet rs = ps.getGeneratedKeys();
             while (rs.next()){
                 long id = rs.getLong(1);
-                person.setId(id);
-                System.out.println(person);
+                entity.setId(id);
+                System.out.println(entity);
             }
             System.out.printf("Records affected: %d%n", recordsAffected);
         } catch (SQLException e) {
             e.printStackTrace();
-            throw new UnableToSaveException("Tried to save person: " + person);
+            throw new UnableToSaveException("Tried to save person: " + entity);
         }
-        return person;
+        return entity;
     }
 
     abstract void mapForSave(T entity, PreparedStatement ps) throws SQLException;
