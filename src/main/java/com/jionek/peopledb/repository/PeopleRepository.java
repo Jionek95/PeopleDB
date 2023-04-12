75ba4cd8e869799995758eb1dcef7e4a4afbcfde
Jionek95 <jakopiczko@wp.pl>
2023-04-12 20:59:54 +0200

    clean up PeopleRepository

--- src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
+++ src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
@@ -6,10 +6,7 @@ import java.math.BigDecimal;
 import java.sql.*;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 import static java.util.stream.Collectors.joining;
 
@@ -18,6 +15,7 @@ public class PeopleRepository extends CRUDRepository<Person> {
     public static final String SAVE_PERSON_SQL = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES(?, ?, ?)";
     public static final String FIND_BY_ID_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE WHERE ID=?";
     public static final String FIND_ALL_SQL = "SELECT * FROM PEOPLE";
+    public static final String SELECT_COUNT_SQL = "SELECT COUNT(*) FROM PEOPLE";
 
     public PeopleRepository(Connection connection) {
         super(connection);
@@ -55,6 +53,11 @@ public class PeopleRepository extends CRUDRepository<Person> {
         return FIND_ALL_SQL;
     }
 
+    @Override
+    protected String getCountSql() {
+        return SELECT_COUNT_SQL;
+    }
+
     /** OVERLOADED METHOD findById(Person person)
 //    public Optional<Person> findById(Person person) {
 //
@@ -78,37 +81,6 @@ public class PeopleRepository extends CRUDRepository<Person> {
 //    }
      **/
 
-
-    public List<Person> findAll() {
-        List<Person> people = new ArrayList<>();
-
-        try {
-            PreparedStatement ps = connection.prepareStatement(FIND_ALL_SQL);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                people.add(extractEntityFromResultSet(rs));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return people;
-    }
-
-
-    public long count() {
-        long count = 0;
-
-        try {
-            PreparedStatement ps = connection.prepareStatement("SELECT COUNT(*) FROM PEOPLE");
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                count = rs.getLong(1);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return count;
-    }
 
     public void delete(Person person) {
         try {
