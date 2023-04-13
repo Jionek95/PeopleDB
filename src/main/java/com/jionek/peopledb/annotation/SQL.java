224b6a382e20e99a4fb89dabd14cde8beeee8822
Jionek95 <jakopiczko@wp.pl>
2023-04-13 15:04:25 +0200

    update SQL annotation to have CrudOperation operationType as well

--- src/main/java/com/jionek/peopledb/annotation/SQL.java
+++ src/main/java/com/jionek/peopledb/annotation/SQL.java
@@ -1,9 +1,12 @@
 package com.jionek.peopledb.annotation;
 
+import com.jionek.peopledb.model.CrudOperation;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SQL {
     String value();
+    CrudOperation operationType();
 }
