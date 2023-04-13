2fff6d30204adde15b544ed78cdf5afbd20cb138
Jionek95 <jakopiczko@wp.pl>
2023-04-13 15:56:36 +0200

    Create new annotation -> MultiSql annotation. That consist SQL[]

---
+++ src/main/java/com/jionek/peopledb/annotation/MultiSql.java
@@ -0,0 +1,9 @@
+package com.jionek.peopledb.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MultiSql {
+    SQL[] value();
+}
