3e687ccb66f2625845f5ef8f4012935cc6aef815
Jionek95 <jakopiczko@wp.pl>
2023-04-13 15:57:00 +0200

    Make @SQl repeatable

--- src/main/java/com/jionek/peopledb/annotation/SQL.java
+++ src/main/java/com/jionek/peopledb/annotation/SQL.java
@@ -2,10 +2,12 @@ package com.jionek.peopledb.annotation;
 
 import com.jionek.peopledb.model.CrudOperation;
 
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(MultiSql.class)
 public @interface SQL {
     String value();
     CrudOperation operationType();
