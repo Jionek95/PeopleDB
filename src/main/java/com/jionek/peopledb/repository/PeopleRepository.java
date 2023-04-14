57ec1f7c4ccd6302ae0d088e3053e04916fd2efb
Jionek95 <jakopiczko@wp.pl>
2023-04-14 11:02:33 +0200

    clean up

--- src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
+++ src/main/java/com/jionek/peopledb/repository/PeopleRepository.java
@@ -36,8 +36,8 @@ public class PeopleRepository extends CRUDRepository<Person> {
         ps.setTimestamp(3, convertDobToTimestamp(entity.getDob()));
         ps.setBigDecimal(4, entity.getSalary());
         ps.setString(5, entity.getEmail());
-
     }
+
     @Override
     @SQL(value = UPDATE_SQL, operationType = CrudOperation.UPDATE)
     void mapForUpdate(Person entity, PreparedStatement ps) throws SQLException {
